9b5b7c8b2f39cabcc2d1734f28ae9c9f6a930b67
Satyajit Sinha <90083670+Dev-Satyajit@users.noreply.github.com>
2025-01-05 10:03:35 +0530

    Test Update

--- src/main/java/com/ssinha/myapp/function/MyAppFunction.java
+++ src/main/java/com/ssinha/myapp/function/MyAppFunction.java
@@ -31,7 +31,15 @@ public class MyAppFunction {
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        log.info("Java HTTP trigger processed a request.");
+		log.info("Java HTTP trigger processed a request.");
+		log.info("Printing environment variables...");
+		System.getenv().forEach((k, v) -> {
+			log.info("{} = {}", k, v);
+		});
+		log.info("Printing system properties...");
+		System.getProperties().forEach((k, v) -> {
+			log.info("{} = {}", k, v);
+		});
 
         return myAppHandler.apply(request);
     }
--- src/main/java/com/ssinha/myapp/service/MyAppService.java
+++ src/main/java/com/ssinha/myapp/service/MyAppService.java
@@ -8,6 +8,7 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.ssinha.common.config.Runtime;
+import com.ssinha.models.MyAppHttpResponse;
 
 @Service
 public class MyAppService {
@@ -21,13 +22,18 @@ public class MyAppService {
 		String testValue = Runtime.get().getConfiguration("test.key");
 		String secretValue = Runtime.get().getConfiguration("test.secret@secure");
 
+		MyAppHttpResponse response = new MyAppHttpResponse();
+		response.setEnvValue(envValue);
+		response.setTestValue(testValue);
+		response.setSecretValue(secretValue);
+
 		if (name == null) {
 			return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
 					.body("Please pass a name on the query string or in the request body")
 					.build();
 		} else {
 			return request.createResponseBuilder(HttpStatus.OK)
-					.body("Hello, " + name + "\nEnv: " + envValue + "\nTestValue: " + testValue + "\nSecret: " + secretValue)
+					.body("Hello, " + name + "\n" + response.toString())
 					.build();
 		}
 	}
