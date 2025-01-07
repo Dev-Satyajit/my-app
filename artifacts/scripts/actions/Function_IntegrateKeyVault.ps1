5d3e444dc0fe9a2decee7b494ea76c3b147a103d
Satyajit Sinha <90083670+Dev-Satyajit@users.noreply.github.com>
2025-01-07 15:15:04 +0530

    Update

--- artifacts/scripts/actions/Function_IntegrateKeyVault.ps1
+++ artifacts/scripts/actions/Function_IntegrateKeyVault.ps1
@@ -14,4 +14,4 @@ $secretId = az keyvault secret show -n $secretName --vault-name $keyvaultname --
 $principalId = az functionapp identity show -n $AppName -g $AppResourceGroupName --query principalId -o tsv
 
 az keyvault set-policy -n $keyvaultname -g $keyvaultRg --object-id $principalId --secret-permissions get
-az functionapp config appsettings set -n $AppName -g $AppResourceGroupName --settings "keyVault_clientKey=@Microsoft.KeyVault(SecretUri=$secretId^^)"
+az functionapp config appsettings set -n $AppName -g $AppResourceGroupName --settings "keyVault_clientKey=@Microsoft.KeyVault(SecretUri=$secretId)"
