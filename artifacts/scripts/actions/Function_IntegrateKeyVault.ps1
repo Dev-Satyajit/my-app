f3c58a967ff67a5ad8c4dbf630549217351e0a47
Satyajit Sinha <90083670+Dev-Satyajit@users.noreply.github.com>
2025-01-08 18:11:25 +0530

    Update Function_IntegrateKeyVault.ps1

--- artifacts/scripts/actions/Function_IntegrateKeyVault.ps1
+++ artifacts/scripts/actions/Function_IntegrateKeyVault.ps1
@@ -10,8 +10,11 @@ $secretName = $Envionment+"-app-registration-key"
 $keyvaultname = "ssappspub-ci-kv-"+$NetworkEnvionment
 $keyvaultRg = 'common-ci-rg-'+$NetworkEnvionment
 
+$ipAddress = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+az keyvault network-rule add -n $keyvaultname -g $keyvaultRg --ip-address $ipAddress
 $secretId = az keyvault secret show -n $secretName --vault-name $keyvaultname --query "id" -o tsv
-$principalId = az functionapp identity show -n $AppName -g $AppResourceGroupName --query principalId -o tsv
+az keyvault network-rule remove -n $keyvaultname -g $keyvaultRg --ip-address $ipAddress
 
+$principalId = az functionapp identity show -n $AppName -g $AppResourceGroupName --query principalId -o tsv
 az keyvault set-policy -n $keyvaultname -g $keyvaultRg --object-id $principalId --secret-permissions get
 az functionapp config appsettings set -n $AppName -g $AppResourceGroupName --settings "keyVault_clientKey=@Microsoft.KeyVault(SecretUri=$secretId)"
